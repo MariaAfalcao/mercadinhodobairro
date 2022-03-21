7daa9e1d63fdc16e5141aa9583603c25055a066f
Maria Aparecida Falcão <mariaaparecidafalcao3@hotmail.com>
2022-03-20 21:43:42 -0300

    Mercadinho do Bairro - v1
    Menu Drawer com 9 categorias: bebidas, carnes,  laticínios, limpeza, higiene,  hortifruti, padaria, pet shop, utilidades domésticas. 
    Uma página com os cards contendo o nome, imagem e valor dos produtos e outra página com a descrição detalhada, imagem e valor dos produtos.
    Outra página com o botão para o paypal. 
    Esse é o início do esqueleto do projeto, nesse momento está sem as rotas e não está caracterizado.

---
+++ etec_c/src/main/java/com/mercadinhodobairro/etec/EtecApplication.java
@@ -0,0 +1,13 @@
+package com.mercadinhodobairro.etec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EtecApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EtecApplication.class, args);
+	}
+
+}
---
+++ etec_c/src/test/java/com/mercadinhodobairro/etec/EtecApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mercadinhodobairro.etec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EtecApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
